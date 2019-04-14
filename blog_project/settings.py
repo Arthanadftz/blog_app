4c4866f989aa4d3dabadd095c575990e5a21303d
Никита Жданов <nikitazdanov@iMac-Nikita.local>
2019-04-14 19:17:23 +0300

    Login/Signup implemented

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
